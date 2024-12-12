a282e3ea3c9fde1894b3a3bdb25221f9c7604734
Ваня Хортоломей <hortolomei1989@gmail.com>
2024-12-12 22:23:08 +0200

    :zap: Second step

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import '@mantine/core/styles.css';
 import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import {MantineProvider} from '@mantine/core';
+import {App} from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+
+ReactDOM.createRoot(rootElement as HTMLElement).render(
+    <React.StrictMode>
+        <MantineProvider>
+            <App/>
+        </MantineProvider>
+    </React.StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import macrosPlugin from 'vite-plugin-babel-macros';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), macrosPlugin()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
+  },
+  css: {
+    modules: {
+      scopeBehaviour: 'local'
+    },
+    preprocessorOptions: {
+      scss: {}
+    }
+  },
+  server: {
+    port: 3030
+  },
+  preview: {
+    port: 8080
+  }
+});
