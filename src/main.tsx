a60e0f74014130148d16604448622f908a3925ee
Ваня <hortolomei1989@gmail.com>
2024-12-22 18:41:13 +0200

    :zap: Fixed enviroment bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import '@mantine/core/styles.css';
-import './index.css'
 import ReactDOM from 'react-dom/client';
-import {MantineProvider} from '@mantine/core';
-import {App} from './App';
+import { MantineProvider } from '@mantine/core';
+import { App } from './app/app';
 
 const rootElement = document.getElementById('root');
 
 ReactDOM.createRoot(rootElement as HTMLElement).render(
-    <React.StrictMode>
-        <MantineProvider>
-            <App/>
-        </MantineProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
 );
